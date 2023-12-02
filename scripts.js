34387e226cc9258acea2bc0f54e42cb94b76a49c
lilumhoho8 <137062782+lilumhoho8@users.noreply.github.com>
2023-12-02 16:56:01 +0800

    Updated html and added css and js

---
+++ scripts.js
@@ -0,0 +1,22 @@
+/*Scripts*/
+function YouTubeRedirect() {
+    window.open("https://www.youtube.com/@lilumhoho8601")}
+    function RedditRedirect() {
+    window.open("https://www.reddit.com/u/lilumhoho8lilumhoho8")
+    }
+    function openCity(evt, cityName) {
+      var i, tabcontent, tablinks;
+      tabcontent = document.getElementsByClassName("tabcontent");
+      for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+      }
+      tablinks = document.getElementsByClassName("tablinks");
+      for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+      }
+      document.getElementById(cityName).style.display = "block";
+      evt.currentTarget.className += " active";
+    }
+    
+    // Get the element with id="defaultOpen" and click on it
+    document.getElementById("defaultOpen").click();
